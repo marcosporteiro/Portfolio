05f7380852b4d975269381b454ba95c801ab65ca
marcosporteiro <miporteiro@hotmail.com>
2021-08-10 01:22:13 -0300

    add flutter_web workflow

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-import 'package:google_fonts/google_fonts.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:mipagina/utils.dart';
 import 'principal.dart';
--- lib/ventana.dart
+++ lib/ventana.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:mipagina/main.dart';
 import 'package:mipagina/responsive.dart';
 import 'menu.dart';

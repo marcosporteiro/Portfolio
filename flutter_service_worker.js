2d94291444e1553f22fbdfa458ad61ae4961b88f
marcosporteiro <marcosporteiro@users.noreply.github.com>
2021-08-16 07:25:47 +0000

    deploy: 062cbed3d5f754a8bf3cce6de1c30c90b9be863e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,16 +29,16 @@ const RESOURCES = {
 "manifest.json": "ecf8d4eea71ae54799788ad9586a3654",
 "version.json": "a8f83101c2f51eceebd1cdcc7b8ff5fc",
 "favicon.ico": "1665e4bb03d060b66d2a82641167a46e",
-"main.dart.js": "a578fb894a8beee11532f6ea4eb4d8b0",
+"main.dart.js": "2ef4ad0e266b536cfcc50e44f1ccfb48",
 "faviconold.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "b2d1b2e9eb6988059f1a8c54efd8711a",
-"/": "b2d1b2e9eb6988059f1a8c54efd8711a"
+"index.html": "ef712e355a6f9f816f8ef23cad36fb09",
+"/": "ef712e355a6f9f816f8ef23cad36fb09"
 };
 
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-// "/",
+  "/",
 "main.dart.js",
 "index.html",
 "assets/NOTICES",

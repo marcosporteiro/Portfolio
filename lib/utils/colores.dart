678ecea3abc4dc005fd733dc6bdb27e761539fb5
marcosporteiro <miporteiro@hotmail.com>
2021-08-23 16:12:43 -0300

    add flutter_web workflow

--- lib/utils/colores.dart
+++ lib/utils/colores.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-const Color colorMenu = Color(0xFF222629);
+//const Color colorMenu = Color(0xFF222629);
+
+const Color colorMenu = Color(0xFF282828);
 const Color colorTemaMenu = Color(0xFF4caf76);
-//Color colorTemaMenu = Color(0xFFED6A5A);
-//Color colorTemaMenu = Color(0xFF5A95ED);
 
 const Color color2 = Color(0xFF759FBC);
 const Color color3 = Color(0xFFF4B860);

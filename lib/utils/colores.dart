f119851c719386ae6ee40c1890d04c69cd650c88
marcosporteiro <miporteiro@hotmail.com>
2021-08-23 16:19:55 -0300

    add flutter_web workflow

--- lib/utils/colores.dart
+++ lib/utils/colores.dart
@@ -5,10 +5,6 @@ import 'package:flutter/material.dart';
 const Color colorMenu = Color(0xFF282828);
 const Color colorTemaMenu = Color(0xFF4caf76);
 
-const Color color2 = Color(0xFF759FBC);
-const Color color3 = Color(0xFFF4B860);
-const Color color4 = Color(0xFF8093F1);
-
 const Color textoGrande = Colors.white;
 Color textoChico = Colors.white.withOpacity(0.5);
 
